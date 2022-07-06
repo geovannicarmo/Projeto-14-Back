4d10ab05290dfcec5d4c67837a60d56e5edcefd3
= <=>
2022-07-06 11:35:58 -0300

    chore: adjust code for deploy on heroku

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors"
+import chalk from "chalk"
+
+dotenv.config();
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+
+
+const mongoClient = new MongoClient(MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db("meu_lindo_banco");
+});
+
+app.post('/teste', (req,res)=>{
+
+    let body=req.body
+
+    db.collection('testes').insertOne({
+        
+        teste: 1
+    })
+
+    db.collection('testes').find().toArray().then((testes)=>console.log(testes))
+
+    res.sendStatus(201)
+})
+
+
+app.listen(PORT,()=>console.log(chalk.yellow(`Server run in port ${PORT}`)))
+
+
+
+
