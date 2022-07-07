0f1958df781da95893f1970caf995ca6575a5f59
Danton Matheus <dantoncosta03@outlook.com>
2022-07-07 16:41:44 -0300

    :memo: Corrige o nome do arquivo da rota insertProducts

--- src/routes/reouteProducts.js
+++
@@ -1,8 +0,0 @@
-import { insertProduct } from '../controllers/productsControllers.js';
-import { Router } from "express";
-
-const router = Router();
-
-router.post('/insertproduct', insertProduct);
-
-export default router;
