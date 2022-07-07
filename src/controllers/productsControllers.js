7f99f321c745037b3e096a8552a2af30d2088130
Danton Matheus <dantoncosta03@outlook.com>
2022-07-07 18:48:54 -0300

    :memo: Corrige a comunicação com o db na rota insertProducts

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -3,7 +3,7 @@ import { db } from "./dbs/mongoDb.js";
 export async function insertProduct(req, res) {
   try {
     const products = req.body;
-    await db.collection('products').insertMany({ products })
+    await db.collection('products').insertMany( products )
     return res.status(201).send("Cadastrado com sucesso.")
   }
   catch (error) {
